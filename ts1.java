09970952ba1dd667554829aed4027c2e3bd0f30e
manjushrikrishna <117737453+manjushrikrishna@users.noreply.github.com>
2025-04-01 15:43:25 +0530

    Create ts1.java

---
+++ ts1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        // Create a scanner object to get user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask the user to enter a number
+        System.out.print("Enter a number to display its multiplication table: ");
+        int number = scanner.nextInt();
+
+        // Print the multiplication table for the given number
+        System.out.println("Multiplication table for " + number + ":");
+        for (int i = 1; i <= 10; i++) {
+            int result = number * i;
+            System.out.println(number + " x " + i + " = " + result);
+        }
+
+        // Close the scanner object
+        scanner.close();
+    }
+}
